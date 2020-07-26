58bac521d64e166a94c740d95eb1e66740cd4a0f
Gabriel Carneiro <gabcarneiro@outlook.com>
2020-07-26 20:02:48 -0300

    Organizes initial project structure

---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_replica/screens/home/home_screen.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
