556bfd91254e151fec2f37941b5df778995ac502
Gabriel Carneiro <gabcarneiro@outlook.com>
2020-07-26 21:06:36 -0300

    Applies default AppTheme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_replica/constants/app_theme.dart';
 import 'package:twitter_replica/screens/home/home_screen.dart';
 
 class AppWidget extends StatelessWidget {
@@ -6,10 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: AppTheme.primary,
       home: HomeScreen(title: 'Flutter Demo Home Page'),
     );
   }
